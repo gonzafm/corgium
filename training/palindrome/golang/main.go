848974654c0c041a4f5f29177d46073b58fa6987
gonzafm <gonzafm@gmail.com>
2025-05-02 19:10:48 -0500

    Solution with unit tests

--- training/palindrome/golang/main.go
+++ training/palindrome/golang/main.go
@@ -24,5 +24,15 @@ func main() {
 
 // isPalindrome checks if the given integer is a palindrome.
 func isPalindrome(x int) bool {
+	number := strconv.Itoa(x)
+	j := len(number)
+	correction := j % 2
+	for i := range j {
+		if i == j-i-correction {
+			return true
+		} else if number[i] != number[j-i-1] {
+			return false
+		}
+	}
 	return false
 }
