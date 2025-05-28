f74d7c30aa6d0c5db5de6fbf09fd2e394efb3c79
Gonzalo Medina <gonzafm@gmail.com>
2025-05-28 10:17:13 -0500

    fixes in gitignore

--- training/jvm/src/main/java/org/corgium/training/Palindrome.java
+++ training/jvm/src/main/java/org/corgium/training/Palindrome.java
@@ -2,19 +2,6 @@ package org.corgium.training;
 
 public class Palindrome {
     
-    // func isPalindrome(x int) bool {
-// 	number := strconv.Itoa(x)
-// 	j := len(number)
-// 	correction := j % 2
-// 	for i := range j {
-// 		if i == j-i-correction {
-// 			return true
-// 		} else if number[i] != number[j-i-1] {
-// 			return false
-// 		}
-// 	}
-// 	return false
-// }
     public boolean isPalindrome(int x) {
         String number = Integer.toString(x);
         int j = number.length();
