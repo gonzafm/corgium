6808cb1a598d9442fb43cf000d098152619049db
Gonzalo Medina <gonzafm@gmail.com>
2025-05-28 16:01:30 -0500

    Creating action for nx builds and fix js test (#11)

--- training/js/jest.config.ts
+++ training/js/jest.config.ts
@@ -5,8 +5,9 @@ module.exports = {
   displayName: 'js',
   preset: '../../jest.preset.js',
   testEnvironment: "node",
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   transform: {
-    "^.+\.tsx?$": ["ts-jest",{}],
+    "^.+\.[tj]sx?$": ["ts-jest",{}],
   },
 };
 
