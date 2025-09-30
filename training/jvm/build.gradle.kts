1af83fff861c60b37ee1bd82b69222a65b4207b3
Gonzalo Medina <gonzafm@gmail.com>
2025-09-30 14:52:38 -0500

    chore: [nx migration] change-plugin-version-0-1-0

--- training/jvm/build.gradle.kts
+++ training/jvm/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     // Apply the application plugin to add support for building a CLI application in Java.
     application
-    id("dev.nx.gradle.project-graph") version("+")
+    id("dev.nx.gradle.project-graph") version("0.1.0")
 }
 allprojects {
     apply {
