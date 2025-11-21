95ca643a3ae2af510b90ceff9a54e47e22c59d00
Gonzalo Medina <gonzafm@gmail.com>
2025-11-21 17:53:31 -0600

    quest first level

---
+++ training/python/tests/test_quest_concatenation.py
@@ -0,0 +1,15 @@
+import pytest
+
+from quest_concatenation import Solution
+
+@pytest.mark.parametrize(
+    "nums, expected",
+    [
+        ([1,2,1],[1,2,1,1,2,1]),
+        ([1,3,2,1],[1,3,2,1,1,3,2,1])
+    ]
+)
+def test_concatenation(nums: list[int],expected: list[int]):
+    solution = Solution()
+    result = solution.getConcatenation(nums)
+    assert result == expected
---
+++ training/python/tests/test_quest_max_consecutive.py
@@ -0,0 +1,12 @@
+import pytest
+from quest_max_consecutive import Solution
+
+@pytest.mark.parametrize(
+    "nums, expected",
+    [([1,1,0,1,1,1],3),
+        ([1,0,1,1,0,1],2)]
+)
+def test_concatenation(nums: list[int],expected: int):
+    solution = Solution()
+    result = solution.findMaxConsecutiveOnes(nums)
+    assert result == expected
